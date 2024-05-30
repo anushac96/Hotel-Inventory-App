b2661d62451db79adb3eedc233fa6647b51b2209
anushac96 <anushac96@gmail.com>
2024-05-30 03:59:43 -0700

    added pipes and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'hinv-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'hinv-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RoomsComponent,CommonModule]
 })
 export class AppComponent {
   title = 'hotel-inventory-app';
+  role='admin';
 }
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './room';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hinv-rooms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css'
+})
+export class RoomsComponent {
+
+
+  hotelName = 'Hilton Hotel';
+  
+  numberOfRooms=10;
+
+  hideRooms = false;
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+  rooms : Room={
+    totalRooms:20,
+    availableRooms:15,
+    bookedRooms:5,
+  };
+
+  roomList : RoomList[]= [
+    {
+      roomNumber:1,
+      roomType: 'Deluxe Room1',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 500,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('01-Feb-2024'),
+      checkoutTime: new Date('01-Mar-2024')
+    },
+    {
+      roomNumber:2,
+      roomType: 'Deluxe Room2',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 600,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('02-Feb-2024'),
+      checkoutTime: new Date('02-Mar-2024')
+    },{
+      roomNumber:3,
+      roomType: 'Deluxe Room3',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 700,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('03-Feb-2024'),
+      checkoutTime: new Date('03-Mar-2024')
+    },{
+      roomNumber:4,
+      roomType: 'Deluxe Room4',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 800,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('01-Feb-2024'),
+      checkoutTime: new Date('01-Mar-2024')
+    },
+    {
+      roomNumber:5,
+      roomType: 'Deluxe Room5',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 900,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('02-Feb-2024'),
+      checkoutTime: new Date('02-Mar-2024')
+    },{
+      roomNumber:6,
+      roomType: 'Deluxe Room6',
+      amenities: 'AC, WiFi, Kitchen, Bathroom',
+      price: 1000,
+      photo: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg',
+      checkInTime: new Date('03-Feb-2024'),
+      checkoutTime: new Date('03-Mar-2024')
+    }
+  ];
+  
+}
